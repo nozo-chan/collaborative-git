e3dc3500f0f274e6458b3e356022a3bdd0486dd4
nozomi Morel <nozomi.morel@gmail.com>
2015-10-08 17:11:53 +0200

    added index file,index.css,java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Homepage</a></li>" +
+"<li><a href='about.html'>information</a></li>" +
+"<li><a href='weightTraining.html'>Donate</a></li>" +
+
+"</ul>";
