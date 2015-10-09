fd835b301bcf2e5d135997b7159b8295f9789d66
nozomi Morel <nozomi.morel@gmail.com>
2015-10-09 14:09:44 +0200

    edited java and index

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Homepage</a></li>""<li><a href='info.html'>Information</a></li>" +
+"<li><a href='index.html'>Homepage</a></li>" +
+"<li><a href='info.html'>Information</a></li>" +
 "<li><a href='getinvolved.html'>Participate</a></li>" +
 
 "</ul>";
