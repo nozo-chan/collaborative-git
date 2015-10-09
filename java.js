71de8adb9734ea702fcd37afca70eb5d95e1687a
Joe Spampinato <jcspampinato@gmail.com>
2015-10-09 10:21:19 -0400

    changed _donate_ to _participate_, capitalized i of information

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Homepage</a></li>" +
-"<li><a href='about.html'>information</a></li>" +
-"<li><a href='weightTraining.html'>Donate</a></li>" +
+"<li><a href='about.html'>Information</a></li>" +
+"<li><a href='weightTraining.html'>Participate</a></li>" +
 
 "</ul>";
